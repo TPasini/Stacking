cefadd0b9f3685098835cf28012f8a9224ca93df
TPasini <53789959+TPasini@users.noreply.github.com>
2021-10-08 14:34:06 +0200

    Move Lib

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 - Thomas Pasini
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Credits for all libraries to F. de Gasperin and H. W. Edler
+
 import os, sys, shutil
 from astropy.table import Table
 from lib import lib_fits as libfits
