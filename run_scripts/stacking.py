6e29ff22bb0e41d95aaa2f04aa5d9a63c083a581
TPasini <53789959+TPasini@users.noreply.github.com>
2021-10-11 13:08:11 +0200

    Fix

--- run_scripts/stacking.py
+++ run_scripts/stacking.py
@@ -294,8 +294,8 @@ class Source_group(object):
         
         header_orig['CRPIX1'] = size_in_pixel/2
         header_orig['CRPIX2'] = size_in_pixel/2
-        header_surv['CDELT2'] = beamspacing.value * (size / size_in_pixel)
-        header_surv['CDELT1'] = - beamspacing.value * (size / size_in_pixel)
+        header_orig['CDELT2'] = beamspacing.value * (size / size_in_pixel)
+        header_orig['CDELT1'] = - beamspacing.value * (size / size_in_pixel)
         
         aif.writeto('Stacking_plots/stack.fits', img_stack_orig, header=header_orig)
 
